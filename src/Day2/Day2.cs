213314afb31af7f168c81feaf5a60368d2ab40c9
GeniusOctopus <k.white221@outlook.com>
2025-12-02 11:34:40 +0100

    Day 2

--- src/Day2/Day2.cs
+++ src/Day2/Day2.cs
@@ -1,9 +1,95 @@
+using System.Numerics;
+
 namespace AoC2025.Day2;
 
 public class Day2
 {
     public void Part1()
     {
-        var input = File.ReadAllLines("Day2/input.txt");
+        var idRanges = File.ReadAllText("Day2/input.txt").Split(',').Select(x => new { firstId = x.Split('-')[0], lastId = x.Split('-')[1] });
+        // Don't know if the sum could be bigger than ulong.MaxValue, so jsut use BigInteger
+        BigInteger sumOfInvalidIds = 0;
+
+        foreach (var idRange in idRanges)
+        {
+            BigInteger currentId = BigInteger.Parse(idRange.firstId);
+            BigInteger lastId = BigInteger.Parse(idRange.lastId);
+            // Bit ugly but yeah
+            string currentIdStr = idRange.firstId;
+
+            while (currentId <= lastId)
+            {
+                // Only ids with an even count of numbers can have a matching left and right sequence
+                if (currentIdStr.Length % 2 != 0)
+                {
+                    currentId++;
+                    currentIdStr = currentId.ToString();
+                    continue;
+                }
+
+                List<string> sequences = SplitIntoSequences(currentIdStr, currentIdStr.Length / 2);
+
+                if (sequences.All(x => x == sequences.First()))
+                {
+                    sumOfInvalidIds += currentId;
+                }
+
+                currentId++;
+                currentIdStr = currentId.ToString();
+            }
+        }
+
+        Console.WriteLine(sumOfInvalidIds);
+    }
+
+    public void Part2()
+    {
+        var idRanges = File.ReadAllText("Day2/input.txt").Split(',').Select(x => new { firstId = x.Split('-')[0], lastId = x.Split('-')[1] });
+        BigInteger sumOfInvaidIds = 0;
+
+        foreach (var idRange in idRanges)
+        {
+            BigInteger currentId = BigInteger.Parse(idRange.firstId);
+            BigInteger lastId = BigInteger.Parse(idRange.lastId);
+            string currentIdStr = currentId.ToString();
+
+            while (currentId <= lastId)
+            {
+                var sequenceRepeats = false;
+
+                for (int i = 1; i < currentIdStr.Length; i++)
+                {
+                    List<string> sequences = SplitIntoSequences(currentIdStr, i);
+
+                    if (sequences.All(x => x == sequences.First()))
+                    {
+                        sequenceRepeats = true;
+                        break;
+                    }
+                }
+
+                if (sequenceRepeats)
+                {
+                    sumOfInvaidIds += currentId;
+                }
+
+                currentId++;
+                currentIdStr = currentId.ToString();
+            }
+        }
+
+        Console.WriteLine(sumOfInvaidIds);
+    }
+
+    private List<string> SplitIntoSequences(string input, int sequenceSize)
+    {
+        List<string> sequences = [];
+
+        for (int i = 0; i < input.Length; i += sequenceSize)
+        {
+            sequences.Add(input.Substring(i, Math.Min(sequenceSize, input[i..].Length)));
+        }
+
+        return sequences;
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -4,3 +4,4 @@ using AoC2025.Day2;
 // new Day1().Part1();
 // new Day1().Part2();
 new Day2().Part1();
+new Day2().Part2();
