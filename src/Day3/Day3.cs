fb9490f6a5ada94ccb07f85ec899bd9f8714161e
GeniusOctopus <58113292+GeniusOctopus@users.noreply.github.com>
2025-12-03 09:41:51 +0000

    day 3 part 2

--- src/Day3/Day3.cs
+++ src/Day3/Day3.cs
@@ -6,128 +6,75 @@ public class Day3
 {
     public void Part1()
     {
-        var input = File.ReadAllLines("Day3/input.txt").Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList());
-        int sum = 0;
+        var batteryBanks = File.ReadAllLines("Day3/input.txt").Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList());
+        BigInteger totalJoltageOutput = 0;
+        var batteryCount = 2;
 
-        foreach (var line in input)
+        foreach (var batteryBank in batteryBanks)
         {
-            int currentMaxFromLeft = 0;
-            int indexOfCurrentMaxFromLeft = -1;
+            List<(string joltage, int battery)> highJoltageBatteries = [];
 
-            for (int i = 0; i < line.Count - 1; i++)
+            for (int i = 0; i < batteryCount; i++)
             {
-                if (line[i] > currentMaxFromLeft)
-                {
-                    currentMaxFromLeft = line[i];
-                    indexOfCurrentMaxFromLeft = i;
-                }
+                highJoltageBatteries.Add(GetMaxBatteryJoltage(batteryBank, GetSlidingWindowBoundaries(batteryBank, highJoltageBatteries, batteryCount)));
             }
 
-            int currentMaxFromRight = 0;
-            int indexOfCurrentMaxFromRight = -1;
-
-            for (int i = line.Count - 1; i > 0; i--)
-            {
-                if (i == indexOfCurrentMaxFromLeft)
-                {
-                    break;
-                }
-
-                if (line[i] > currentMaxFromRight)
-                {
-                    currentMaxFromRight = line[i];
-                    indexOfCurrentMaxFromRight = i;
-                }
-            }
-
-            sum += int.Parse($"{currentMaxFromLeft}{currentMaxFromRight}");
+            totalJoltageOutput += BigInteger.Parse(string.Join("", highJoltageBatteries.Select(x => x.joltage)));
         }
 
-        Console.WriteLine(sum);
+        Console.WriteLine(totalJoltageOutput);
     }
 
     public void Part2()
     {
-        var input = File.ReadAllLines("Day3/input.txt").Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList());
-        BigInteger sum = 0;
+        var batteryBanks = File.ReadAllLines("Day3/input.txt").Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList());
+        BigInteger totalJoltageOutput = 0;
+        var batteryCount = 12;
 
-        foreach (var line in input)
+        foreach (var batteryBank in batteryBanks)
         {
-            List<(string jolt, int index)> jolts = [];
+            List<(string joltage, int battery)> highJoltageBatteries = [];
 
-            for (int count = 0; count < 12; count++)
+            for (int i = 0; i < batteryCount; i++)
             {
-                int endIndex = line.Count - 11;
-                endIndex += jolts.Count;
-                if (endIndex >= line.Count)
-                {
-                    endIndex = line.Count;
-                }
-
-                int startIndex = 0;
-
-                if (jolts.Count != 0)
-                {
-                    startIndex = jolts.Last().index + 1;
-                }
-                
-
-                var subset = line[startIndex..endIndex];
-
-                int currentMaxFromLeft = 0;
-                int indexOfCurrentMaxFromLeft = -1;
-
-                for (int i = 0; i < subset.Count - 1; i++)
-                {
-                    if (subset[i] > currentMaxFromLeft)
-                    {
-                        currentMaxFromLeft = subset[i];
-                        indexOfCurrentMaxFromLeft = i;
-                    }
-                }
-
-                // int currentMaxFromRight = 0;
-                // int indexOfCurrentMaxFromRight = -1;
-                // int minAvailableIndex = line.Count - 1;
-                // minAvailableIndex -= minAvailableIndex - 12;
-                // minAvailableIndex -= jolts.Count;
-
-                // for (int i = line.Count - 1; i >= 11 - jolts.Count; i--)
-                // {
-                //     if (jolts.Any(x => x.index == i))
-                //     {
-                //         continue;
-                //     }
-
-                //     if (line[i] > currentMaxFromRight)
-                //     {
-                //         currentMaxFromRight = line[i];
-                //         indexOfCurrentMaxFromRight = i;
-                //     }
-                // }
-
-                jolts.Add((currentMaxFromLeft.ToString(), indexOfCurrentMaxFromLeft));
-
-                // int currentMaxFromLeft = 0;
-                // int indexOfCurrentMaxFromLeft = -1;
-
-                // for (int i = 0; i < line.Count - 1; i++)
-                // {
-                //     if (line[i] > currentMaxFromLeft)
-                //     {
-                //         currentMaxFromLeft = line[i];
-                //         indexOfCurrentMaxFromLeft = i;
-                //     }
-                // }
+                highJoltageBatteries.Add(GetMaxBatteryJoltage(batteryBank, GetSlidingWindowBoundaries(batteryBank, highJoltageBatteries, batteryCount)));
             }
 
-            jolts.Reverse();
-            var asd = string.Join("", jolts.Select(x => x.jolt));
-            sum += BigInteger.Parse($"{asd}");
+            totalJoltageOutput += BigInteger.Parse(string.Join("", highJoltageBatteries.Select(x => x.joltage)));
         }
 
-        Console.WriteLine(sum);
+        Console.WriteLine(totalJoltageOutput);
+    }
+
+    private (int windowStartIndex, int windowEndIndex) GetSlidingWindowBoundaries(List<int> batteryBank, List<(string joltage, int battery)> highJoltageBatteries, int batteryCount)
+    {
+        int windowStartIndex = highJoltageBatteries.Count != 0 ? highJoltageBatteries.Last().battery + 1 : 0;
+        int windowEndIndex = batteryBank.Count - (batteryCount - 1);
+        windowEndIndex += highJoltageBatteries.Count;
+        if (windowEndIndex >= batteryBank.Count)
+        {
+            windowEndIndex = batteryBank.Count;
+        }
+
+        return (windowStartIndex, windowEndIndex);
+    }
+
+    private (string joltage, int battery) GetMaxBatteryJoltage(List<int> line, (int startIndex, int endIndex) slidingWindowBoundaries)
+    {
+        var subset = line[slidingWindowBoundaries.startIndex..slidingWindowBoundaries.endIndex];
+
+        int maxJoltage = 0;
+        int maxJoltageBattery = -1;
+
+        for (int i = 0; i < subset.Count; i++)
+        {
+            if (subset[i] > maxJoltage)
+            {
+                maxJoltage = subset[i];
+                maxJoltageBattery = i + slidingWindowBoundaries.startIndex;
+            }
+        }
+
+        return (maxJoltage.ToString(), maxJoltageBattery);
     }
 }
-
-// 987654321111 + 811111111119 + 434234234278 + 888911112111 = 3121910778619
--- src/Program.cs
+++ src/Program.cs
@@ -6,5 +6,5 @@ using AoC2025.Day3;
 // new Day1().Part2();
 // new Day2().Part1();
 // new Day2().Part2();
-// new Day3().Part1();
+new Day3().Part1();
 new Day3().Part2();
