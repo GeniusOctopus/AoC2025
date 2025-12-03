7376ecc4b0497b1e23fc5f8c9abbf643c8e5d491
GeniusOctopus <58113292+GeniusOctopus@users.noreply.github.com>
2025-12-03 12:18:51 +0000

    day 3 cleanup

--- src/Day3/Day3.cs
+++ src/Day3/Day3.cs
@@ -4,58 +4,28 @@ namespace AoC2025.Day3;
 
 public class Day3
 {
-    public void Part1()
+    public void Part1And2(int batteryCount)
     {
         var batteryBanks = File.ReadAllLines("Day3/input.txt").Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList());
         BigInteger totalJoltageOutput = 0;
-        var batteryCount = 2;
 
         foreach (var batteryBank in batteryBanks)
         {
             List<(string joltage, int battery)> highJoltageBatteries = [];
 
             for (int i = 0; i < batteryCount; i++)
-            {
                 highJoltageBatteries.Add(GetMaxBatteryJoltage(batteryBank, GetSlidingWindowBoundaries(batteryBank, highJoltageBatteries, batteryCount)));
-            }
 
             totalJoltageOutput += BigInteger.Parse(string.Join("", highJoltageBatteries.Select(x => x.joltage)));
         }
-
-        Console.WriteLine(totalJoltageOutput);
-    }
-
-    public void Part2()
-    {
-        var batteryBanks = File.ReadAllLines("Day3/input.txt").Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList());
-        BigInteger totalJoltageOutput = 0;
-        var batteryCount = 12;
-
-        foreach (var batteryBank in batteryBanks)
-        {
-            List<(string joltage, int battery)> highJoltageBatteries = [];
-
-            for (int i = 0; i < batteryCount; i++)
-            {
-                highJoltageBatteries.Add(GetMaxBatteryJoltage(batteryBank, GetSlidingWindowBoundaries(batteryBank, highJoltageBatteries, batteryCount)));
-            }
-
-            totalJoltageOutput += BigInteger.Parse(string.Join("", highJoltageBatteries.Select(x => x.joltage)));
-        }
-
         Console.WriteLine(totalJoltageOutput);
     }
 
     private (int windowStartIndex, int windowEndIndex) GetSlidingWindowBoundaries(List<int> batteryBank, List<(string joltage, int battery)> highJoltageBatteries, int batteryCount)
     {
         int windowStartIndex = highJoltageBatteries.Count != 0 ? highJoltageBatteries.Last().battery + 1 : 0;
-        int windowEndIndex = batteryBank.Count - (batteryCount - 1);
-        windowEndIndex += highJoltageBatteries.Count;
-        if (windowEndIndex >= batteryBank.Count)
-        {
-            windowEndIndex = batteryBank.Count;
-        }
-
+        int windowEndIndex = batteryBank.Count - (batteryCount - 1) + highJoltageBatteries.Count;
+        windowEndIndex = windowEndIndex >= batteryBank.Count ? batteryBank.Count : windowEndIndex;
         return (windowStartIndex, windowEndIndex);
     }
 
@@ -63,17 +33,14 @@ public class Day3
     {
         var subset = line[slidingWindowBoundaries.startIndex..slidingWindowBoundaries.endIndex];
 
-        int maxJoltage = 0;
-        int maxJoltageBattery = -1;
+        (int maxJoltage, int maxJoltageBattery) = (0, -1);
 
         for (int i = 0; i < subset.Count; i++)
-        {
             if (subset[i] > maxJoltage)
             {
                 maxJoltage = subset[i];
                 maxJoltageBattery = i + slidingWindowBoundaries.startIndex;
             }
-        }
 
         return (maxJoltage.ToString(), maxJoltageBattery);
     }
